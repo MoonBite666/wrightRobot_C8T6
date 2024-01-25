32f23ef41f65f8a84ff82408255c6a557108a4e3
MoonFeather <2214453884@qq.com>
2024-01-25 14:30:09 +0800

    Modified motor function location Added D80NK sensors' code

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -78,13 +78,9 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PBPin PBPin */
   GPIO_InitStruct.Pin = D80NK2_Pin|D80NK1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
